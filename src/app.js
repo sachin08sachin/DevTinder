7610901af023ed3c083f3d64987d8ac4ab1d258b
sachin08sachin <sgsachin359@gmail.com>
2025-09-29 19:33:38 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -23,6 +23,44 @@ app.post("/signup", async (req,res)=> {
         res.status(500).send("Internal server error");
     }
 })
+//GET user by emailId 
+app.get("/user", async (req,res) => {
+    const userEmail = req.body.emailId;
+    try{
+        const users = await User.find({emailId: userEmail});
+        if(users.length === 0){
+            res.status(404).send("User not found");
+        }
+        res.send(users);
+    }
+    catch(err) {
+        res.status(400).send("Error in fetching user");
+    }
+})
+//FEED API - GET / FEED get all users from dtatabase
+app.get("/feed", async (req, res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(500).send("Internal server error");
+    }
+})
+//if  two users have same mailId -> gives an object not an array
+app.get("/user", async(req,res) => {
+    const userEmail = req.body.emailId;
+    try{
+        const users = await User.findOne({emailId: userEmail});
+        if(!users){
+            res.status(404).send("user not found");
+        }
+        res.send(users);
+    }
+    catch(err){
+        res.status(500).send("Internal server error");
+    }
+})
 
 connectDB()
    .then(()=> {
