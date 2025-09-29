08b882b962af5e586b4a93dc6c1af5f8f0024df3
sachin08sachin <sgsachin359@gmail.com>
2025-09-29 10:55:06 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
-app.use("/test",(req,res)=> {
-    res.send("hello world");
+
+//app.use("\route", rH1, [rH2, rH3], rH4); -> works fine
+app.use("/user",[(req,res, next)=> {
+    //  res.send("hello world from user 1");
+    console.log("request handler user 1");
+   next();
+},(req,res,next)=> {
+    console.log("request handler user 2");
+    // res.send("hellow");
+    next();
+}],(req,res,next)=> {
+    console.log("request handler user 3");
+   // res.send("hellow 3");
+   next();
+},(req,res,next)=> {
+    console.log("request handler user 4");
+    res.send("hellow 4");
+    // next();
 });
-app.listen(3000, ()=> {
-    console.log("listening on port 3000");
+app.listen(7777, ()=> {
+    console.log("listening on port 7777");
 });
