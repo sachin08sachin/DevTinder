87fb442c6314860db2cbefb54b24838262db6902
sachin08sachin <sgsachin359@gmail.com>
2025-09-29 12:38:04 +0530

    Error-Handling

--- src/app.js
+++ src/app.js
@@ -1,31 +1,23 @@
 const express = require('express');
 const app = express();
 
-const {adminAuth, userAuth} = require("./middlewares/auth.js");
+app.get("/getUserData", (req,res)=> {
+    try{
+        //Logic to connect DB and getUserData
+        throw new Error("DB connection failed");
+        res.status(200).send("User data");
+    }
+    catch(err) {
+        res.status(500).send("Error found");
+    }
+})
 
-app.use("/admin", adminAuth);
-app.get("/admin/getAllData", (req,res, next) => {
-    console.log("admin getAllData route handler called");
-    res.send("admin getAllData route handler called");
-})
-app.get("/admin/deleteAllData", (req, res, next) => {
-    console.log("admin deleteAllData route handler called");
-    res.send("admin deleteAllData route handler called");
-})
-//app.use("/user", userAuth);
-//app.use("\route", rH1, [rH2, rH3], rH4); -> works fine
-app.post("/user/login", (req,res)=> {
-    res.send("user login route handler called");
-})
-app.get("/user/getuser",userAuth,(req,res,next)=> {
-    console.log("print user 1!");
-    res.send("get user data");
-    
+//Always keep last of the application ("/")
+app.use((err, req,res, next) => {
+    if(err){
+        res.status(500).send("Some error occurred");
+    }
 });
-app.get("/user/deleteuser",userAuth, (req,res,next)=> {
-    console.log("print user 2!");
-  res.send("delete user data");
-})
 app.listen(7777, ()=> {
     console.log("listening on port 7777");
 });
